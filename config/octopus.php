63452cab1b8660fad2502f93f6919d8b60f07d21
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2024-06-17 20:35:10 +0100

    Add Octopus api config

---
+++ config/octopus.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'api_key' => env('OCTOPUS_API_KEY'),
+    'export_mpan' => env('OCTOPUS_EXPORT_MPAN'),
+    'import_mpan' => env('OCTOPUS_IMPORT_MPAN'),
+    'export_serial_number' => env('OCTOPUS_EXPORT_SERIAL_NUMBER'),
+    'import_serial_number' => env('OCTOPUS_IMPORT_SERIAL_NUMBER'),
+];
