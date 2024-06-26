02855d509cc87d27ee39a093ed8668f866aefaaa
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2024-06-26 21:28:41 +0100

    Create Inverter import and test

---
+++ app/Imports/InverterImport.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Inverter;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Imports\HeadingRowFormatter;
+
+/**
+ * Row 8 is the heading
+ * Time [Column B] -> Period - converted to 30 min time periods
+ * Today Yield(kWh) -> yield - PV yield for the period
+ * Daily Energy to Grid(kWh) -> to_grid - Excess energy send to the grid for the period
+ * Daily Energy from Grid(kWh) = from_grid - Import energy imported from the grid
+ * Daily Consumption Energy(kWh) = consumption - Energy consumed during the period
+ *
+ * (new InverterImport)->import('inverter.xls', null, \Maatwebsite\Excel\Excel::XLS);
+ * $collection = (new InverterImport)->toCollection('inverter.xlsx');
+ * Possibly:
+ * $collection = (new InverterImport)->toCollection('inverter.xlsx', null, \Maatwebsite\Excel\Excel::XLS);
+ */
+class InverterImport implements ToCollection, WithHeadingRow
+{
+    public function __construct()
+    {
+        HeadingRowFormatter::default(HeadingRowFormatter::FORMATTER_NONE);
+    }
+
+    /**
+     * @param Collection $collection
+     */
+    public function collection(Collection $collection)
+    {
+        if ($collection->isEmpty()) {
+            return;
+        }
+
+        // 0 index
+        $collectionCount = $collection->count() -1;
+
+        $firstInPeriod = $collection[0];
+
+        $dateTimeWithoutTZ = substr($collection[0]['Time'], 0, 19);
+        $carbonInstance = Carbon::createFromFormat('d/m/Y H:i:s', $dateTimeWithoutTZ, 'UTC');
+        assert( $carbonInstance instanceof Carbon);
+        $currentPeriod = $carbonInstance->timezone('UTC')->startOfHour();
+        $nextPeriod = $currentPeriod->clone()->addMinutes(30);
+
+        $data = [];
+        foreach ($collection as $i => $row) {
+
+            $dateTimeWithoutTZ = substr($row['Time'], 0, 19); // '23/06/2024 00:00:10'
+            $carbonInstance = Carbon::createFromFormat('d/m/Y H:i:s', $dateTimeWithoutTZ, 'UTC');
+            assert( $carbonInstance instanceof Carbon);
+
+            if ($carbonInstance > $nextPeriod || $i === $collectionCount) {
+
+                $lastInPeriod = $row;
+
+                $data[] = [
+                    'period' => $currentPeriod->toDateTimeString(),
+                    'yield' => $lastInPeriod['Today Yield(kWh)'] - $firstInPeriod['Today Yield(kWh)'],
+                    'to_grid' => $lastInPeriod['Daily Energy to Grid(kWh)'] - $firstInPeriod['Daily Energy to Grid(kWh)'],
+                    'from_grid' => $lastInPeriod['Daily Energy from Grid(kWh)'] - $firstInPeriod['Daily Energy from Grid(kWh)'],
+                    'consumption' => $lastInPeriod['Daily Consumption Energy(kWh)'] - $firstInPeriod['Daily Consumption Energy(kWh)'],
+                ];
+                // Log::info('First in period', ['index' => $i, 'Last in period' => $lastInPeriod['Daily Energy to Grid(kWh)'], 'First in period' => $firstInPeriod['Daily Energy to Grid(kWh)']]);
+
+                $currentPeriod->addMinutes(30);
+                $nextPeriod->addMinutes(30);
+
+                $firstInPeriod = $row;
+            }
+
+            // this is a test implementation, the final one would aggregate the usage for the 30 min and add it to the
+            // start time for that period
+
+        }
+        // Log::info('Saving excel', ['data' => $data]);
+        Inverter::upsert(
+            $data,
+            uniqueBy: ['period'],
+            update: ['yield', 'to_grid', 'from_grid', 'consumption']
+        );
+    }
+
+    public function headingRow(): int
+    {
+        return 8;
+    }
+}
---
+++ tests/Feature/Imports/InverterImportTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Imports;
+
+use App\Imports\InverterImport;
+use App\Models\Inverter;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class InverterImportTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_an_inverter_import_can_create_inverter_data(): void
+    {
+        Excel::import(
+            new InverterImport(),
+            'tests/Inverter History Report_20240625044020_1300386381676952160.xls'
+            , null,
+            \Maatwebsite\Excel\Excel::XLS
+        );
+        // Data from 2024-06-22, yield 17.6, to grid 8.4, imported 1.8, consumption 10.9
+
+        $inverters = Inverter::all();
+
+        $this->assertEqualsWithDelta(17.6, $inverters->sum('yield'), 0.001 );
+        $this->assertEqualsWithDelta(8.4, $inverters->sum('to_grid'), 0.001 );
+        $this->assertEqualsWithDelta(10.9, $inverters->sum('consumption'), 0.001 );
+        $this->assertEqualsWithDelta(1.8 , $inverters->sum('from_grid'), 0.001 );
+    }
+
+    public function test_a_inverter_can_be_upserted_for_the_same_period(): void
+    {
+        Excel::import(
+            new InverterImport(),
+            'tests/Inverter History Report_20240625044020_1300386381676952160.xls'
+            , null,
+            \Maatwebsite\Excel\Excel::XLS
+        );
+
+        $this->assertDatabaseCount('inverters', 48);
+
+        Excel::import(
+            new InverterImport(),
+            'tests/Inverter History Report_20240625044020_1300386381676952160.xls'
+            , null,
+            \Maatwebsite\Excel\Excel::XLS
+        );
+
+        $this->assertDatabaseCount('inverters', 48);
+    }
+}
